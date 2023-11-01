c6c39fa7493c03a85dd4def9642b1347cbf14954
Juan Boullosa <Jmb0507@gmail.com>
2023-11-01 19:31:42 +0100

    Comments added to 1,2,3
    Changed filename
    
    E3 Finished?
    
    Create README.md
    
    Update README.md
    
    upload paper
    
    A2 task
    
    E4 Finished
    
    E4 description
    
    cooment
    
    jupyter
    
    Critical changes
    
    E5 finished
    
    exceptions
    
    compact vars1
    
    Extra comments and formatting
    
    erase
    
    equations LaTeX
    
    E7 finished
    
    outputs
    
    Execises Finished
    
    C'est Fini
    
    finished
    
    A2 Open
    
    A2
    
    v2
    
    ... some changes???
    
    A3
    
    restructuring filesystem A3
    
    FINAL VERSION without EXTRAS A1
    
    A2-E1 Finished?
    
    CLASS migration, E1 Finished.
    
    -
    
    Output
    
    output E1
    
    E2. Database FINISHED
    
    -
    
    Reverse E2 - to not OO programming
    
    E3 Finished (Poem generator)
    
    E4 Finished (Phone Book)
    
    E6 Finished, E5 still needs work
    
    E5 String Analysis finished. A2 Sheet Completed
    
    A3 Skeleton upload
    
    A3 Initialisation
    
    New_Combined_A1_A3_notebook
    
    random assigment

---
+++ assign_ex.py
@@ -0,0 +1,14 @@
+import random
+
+people = ["Juan", "Jiah", "Julia", "Phillip", "Neeha"]
+exercises = ["E4", "E5", "E6", "E7", "E8"]
+
+assignments = {}
+
+for exercise in exercises:
+    random_person = random.choice(people)
+    assignments[exercise] = random_person
+    people.remove(random_person)
+
+for exercise, person in assignments.items():
+    print(f"{exercise} is assigned to {person}")
