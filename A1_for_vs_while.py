4add5efa4cbf96c261567bf6ebb8c4f43b8176fa
Juan Manuel Boullosa Novo <Jmb0507@gmail.com>
2023-11-29 12:03:57 +0100

    A1 for vs while vs comprehesion comparison efficency

---
+++ A1_for_vs_while.py
@@ -0,0 +1,63 @@
+import time
+
+def wind_speed(u_0, ct, D, sigma):
+    # Calculate wind speed at a certain distance behind a turbine based on the wake model equation
+    a = 1 - (ct * D ** 2) / (8 * sigma ** 2)
+    return u_0 * a ** 0.5
+
+def sigma(k, x, beta, D):
+    # Calculate sigma value based on the wake model equation
+    return k * x + 0.25 * beta ** 0.5 * D
+
+# Turbine parameters
+uniform_wind_speed = 9  # m/s
+D = 154  # m
+ct = 0.763
+k = 0.02
+beta = 0.5 * (1 + (1 - ct) ** 0.5) / ((1 - ct) ** 0.5)
+
+# Threshold parameters
+threshold = 8.95 # m/s
+increment = 1  # m
+
+def method_for(k, beta, D, ct, threshold, increment):
+    start_time = time.time()
+    # Calculate wind speed up to a certain distance behind the turbine when wind speed is below threshold
+    for x in range(0, int(1e5), increment):
+        sigma_value = sigma(k, x, beta, D)
+        u = wind_speed(uniform_wind_speed, ct, D, sigma_value)
+        
+        if u >= threshold:
+            break
+    end_time = time.time()
+    return end_time - start_time
+        
+def method_while(k, beta, D, ct, threshold, increment):
+    start_time = time.time()
+    x = 0
+    while x < int(1e5):
+        sigma_value = sigma(k, x, beta, D)
+        u = wind_speed(uniform_wind_speed, ct, D, sigma_value)
+        
+        if u >= threshold:
+            break
+        x += increment
+    end_time = time.time()
+    return end_time - start_time
+
+def method_pythonic(k, beta, D, ct, threshold, increment):
+    start_time = time.time()
+    x = next((x for x in range(0, int(1e5), increment) if wind_speed(uniform_wind_speed, ct, D, sigma(k, x, beta, D)) >= threshold), None)    
+    end_time = time.time()
+    return end_time - start_time
+
+number_times = 1000
+method_1_times = [method_for(k, beta, D, ct, threshold, increment)  for i in range(number_times)]
+method_2_times = [method_while(k, beta, D, ct, threshold, increment)  for i in range(number_times)]
+method_3_times = [method_pythonic(k, beta, D, ct, threshold, increment)  for i in range(number_times)]
+
+print('Method 1: ', sum(method_1_times)/len(method_1_times))
+print('Method 2: ', sum(method_2_times)/len(method_2_times))
+print('Method 3: ', sum(method_3_times)/len(method_3_times))
+
+print('Best method is the: ', ['first', 'second', 'third'][[sum(method_1_times)/len(method_1_times), sum(method_2_times)/len(method_2_times), sum(method_3_times)/len(method_3_times)].index(min([sum(method_1_times)/len(method_1_times), sum(method_2_times)/len(method_2_times), sum(method_3_times)/len(method_3_times)]))])
