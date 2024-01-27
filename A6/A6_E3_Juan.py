4fefffa7ddd7b9bc9c9ea85382e8fa3158402d2b
Juan Manuel Boullosa Novo <Jmb0507@gmail.com>
2024-01-27 19:02:00 +0100

    A6_finito

--- A6/A6_E3_Juan.py
+++ A6/A6_E3_Juan.py
@@ -1,68 +1,19 @@
-############################################ JULIA E1 PART ############################################
-import numpy as np
 import pandas as pd
+import numpy as np
 
-#########################################E1.a#########################################
-random_array = np.random.randint(0, 20, size=(2, 24)) #array shape: (2, 24)
-#print(random_array)
+# Read previously defined csv file with Error-Log column
+df = pd.read_csv('series_df2.csv', delimiter=';')
+# Replace NaN values in 'Error-Log' column with '0kay'
+df['Error-Log'] = df['Error-Log'].fillna('0kay')
+print(df, '\n')
 
-#########################################E1.b#########################################
-# directly from the array
-df_direct = pd.DataFrame(random_array.T, columns=['Data1', 'Data2']) #T. for transpose 
+# Group by 'Error-Log' and calculate mean of 'Data 1' and 'Data 2'
+grouped_df = df.groupby('Error-Log')[['Data 1', 'Data 2']].mean()
+print(grouped_df, '\n')
 
-#dict from np.array 
-random_dict = {}
-for index, row in enumerate(random_array):
-    random_dict[f"Data {index + 1}"] = row
-df_dict = pd.DataFrame(random_dict)
+# Set the index to the square product of the current index
+df.index = df.index ** 2
 
-
-# from two pd.Series objects created from the individual array columns
-serie1_df = pd.Series(random_array[1,:])
-serie2_df = pd.Series(random_array[0,:])
-
-series_combined  = pd.DataFrame() #empty data frame 
-# Now we can add the columns one-by-one:
-series_combined["Data 1"] = serie1_df
-series_combined["Data 2"] = serie2_df
-
-print(f"E1.b1\n:{df_direct}") #E1.b1
-print()
-print(f"E1.b2\n:{df_dict}")  #E1.b2
-print()
-print(f"E1.b3\n:{series_combined}") #E1.b3
-#########################################E1.c#########################################
-
-series_combined["DataProd"] = series_combined["Data 1"]*series_combined["Data 2"]
-print("E1.c")
-print(series_combined)
-############################################ JULIA E2 PART ############################################
-#########################################E2.a#########################################
-series_combined = series_combined.astype(np.float64)
-#print(series_combined.dtypes) #print data type for whole dataframe
-
-#########################################E2.b#########################################
-series_combined.to_csv('series_df.csv', index=False, float_format='%.2f') #index= True or False to include / exclude the index column 
-
-#########################################E2.d#########################################
-df = pd.read_csv('series_df.csv', delimiter=';')
-
-# Print the DataFrame
+# Replace the rows in 'Data 1' and 'Data 2' that have 'Error' in 'Error-Log' with np.nan
+df.loc[df['Error-Log'] == 'Error', ['Data 1', 'Data 2']] = np.nan
 print(df)
-
-#what happened with the blank rows? 
-print()
-print(f"Data type of Error-cell: {type(df['Error-Log'][2])}") 
-print(f"Data type of blank cell: {type(df['Error-Log'][0])}")  
-print(f"Data type new column: {type(df['Error-Log'])}")
-########################################################################################
-
-############################################ JUAN E3 PART ############################################
-# Replace all nan values in Error-Log column by 0kay:
-# Use group functionality to group data into 0kay and Error and
-# calculate the mean values of both Data-columns when grouped
-# Set the index in df to the square product of the current index
-# Replace the rows in the Data1, Data2 columns thet have an Error
-# entry in the Error-Log column by np.nan (use only 1 line)
-########################################################################################
-
